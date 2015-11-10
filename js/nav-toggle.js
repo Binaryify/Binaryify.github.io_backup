6f1b019b8ae1fcc0db73a696361c03b736c22316
zhuangtongfa <zhuangtongfa@qq.com>
2015-11-10 17:22:36 +0800

    Site updated: 2015-11-10 17:22:34

--- js/nav-toggle.js
+++ js/nav-toggle.js
@@ -10,4 +10,38 @@ $(document).ready(function () {
       $siteNav[animateCallback](ON_CLASS_NAME);
     });
   });
+
+  //标题切换
+  	var hidden, state, visibilityChange,title=document.title; 
+if (typeof document.hidden !== "undefined") {
+	hidden = "hidden";
+	visibilityChange = "visibilitychange";
+	state = "visibilityState";
+} else if (typeof document.mozHidden !== "undefined") {
+	hidden = "mozHidden";
+	visibilityChange = "mozvisibilitychange";
+	state = "mozVisibilityState";
+} else if (typeof document.msHidden !== "undefined") {
+	hidden = "msHidden";
+	visibilityChange = "msvisibilitychange";
+	state = "msVisibilityState";
+} else if (typeof document.webkitHidden !== "undefined") {
+	hidden = "webkitHidden";
+	visibilityChange = "webkitvisibilitychange";
+	state = "webkitVisibilityState";
+}
+
+// 添加监听器，在title里显示状态变化
+document.addEventListener(visibilityChange, function() {
+	
+	if(document[state]=="hidden"){
+		document.title = "出bug了,快看!";
+	}else{
+		document.title = title;
+	}
+}, false);
+
+// 初始化
+// document.title = document[state];
+
 });
